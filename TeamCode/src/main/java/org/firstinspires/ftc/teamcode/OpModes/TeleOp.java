4e1f628bf36dd1086534f62786f33cdd611f837e
sparky520 <144406148+sparky520@users.noreply.github.com>
2023-09-29 17:49:12 -0500

    did mybest :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-public class TeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.subsystem.mecanum;
+
+public class TeleOp extends OpMode {
+    mecanum wheels;
+    @Override
+    public void init() {
+        double mul = 1.1;
+        wheels.fieldCentric(mul);
+    }
+
+    @Override
+    public void loop() {
+
+        if(gamepad1.dpad_up) {
+            wheels.resetIMU();
+        }
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/mecanum.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,70 +12,58 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
-public class mecanum extends OpMode {
-    DcMotor frontLeftMotor;
-    DcMotor frontRightMotor;
-    DcMotor backLeftMotor;
-    DcMotor backRightMotor;
-    double frontLeftPower=0.0;
-    double frontRightPower=0.0;
-    double backLeftPower=0.0;
-    double backRightPower=0.0;
+public class mecanum extends OpMode{
     IMU imu;
-
-
-    @Override
-    public void init() {
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-
+    DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+    DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+    DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+    DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+    public mecanum(){
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         IMU imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         imu.initialize(parameters);
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    @Override
-    public void init_loop() {
 
+
+
+    public void resetIMU(){
+        imu.resetYaw();
     }
-
-    @Override
-    public void start(){
-
-
-
-
-    }
-    @Override
-    public void loop() {
-
-
-        double y = -gamepad1.left_stick_y;
+    public void fieldCentric(double mult){
+        double y = gamepad1.left_stick_y;
         double x = -gamepad1.left_stick_x;
         double rx = -gamepad1.right_stick_x;
         double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        if (gamepad1.options) {
-            imu.resetYaw();
-        }
+
         double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
         double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-        rotX = rotX * 1.1;
+
         double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-        double frontLeftPower = (rotY + rotX + rx) / denominator;
-        double backLeftPower = (rotY - rotX + rx) / denominator;
-        double frontRightPower = (rotY - rotX - rx) / denominator;
-        double backRightPower = (rotY + rotX - rx) / denominator;
+        double frontLeftPower =1 * (mult * (rotY + rotX + rx)) / denominator;
+        double backLeftPower = 1 * (mult * (rotY - rotX + rx)) / denominator;
+        double frontRightPower = 1 * (mult * (rotY - rotX - rx)) / denominator;
+        double backRightPower = 1 * (mult * (rotY + rotX - rx)) / denominator;
 
         frontLeftMotor.setPower(frontLeftPower);
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
+    }
+
+    public void init() {
 
     }
+
+
+    public void loop() {
+
+    }
+
+
+
 }
